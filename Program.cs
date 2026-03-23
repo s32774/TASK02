f342dc67db126e843b22b365bac3cef307ca2972
Olena Chornobai <s32774@pjwstk.edu.pl>
2026-03-23 18:17:07 +0100

    add program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,43 @@
-﻿
-
-using APBD_TASK2.Database;
+﻿using APBD_TASK2.Database;
+using APBD_TASK2.Enum;
+using APBD_TASK2.Models;
+using APBD_TASK2.Services;
 
 var db = Singleton.Instance;
+var service = new RentalService();
+var user1 = new User("olena", "Chornobai", UserType.Student);
+var user2 = new User("Alex", "Ruban", UserType.Employee);
 
+service.AddUser(user1);
+service.AddUser(user2);
+var laptop = new Laptop("Lenovo", "Office laptop", 16, "i7");
+var camera = new Camera("Canon", "Photo camera", 24, true);
+var projector = new Projector("Epson", "HD projector", 3000, true);
+
+service.AddEquipment(laptop);
+service.AddEquipment(camera);
+service.AddEquipment(projector);
+
+Console.WriteLine("All equipment:");
+foreach (var e in service.GetAllEquipment())
+{
+    Console.WriteLine(e.GetInfo());
+}
+
+Console.WriteLine("Renting laptop to student");
+service.RentEquipment(user1.Id, laptop.Id, 3);
+try
+{
+    Console.WriteLine("Trying to rent same laptop again");
+    service.RentEquipment(user2.Id, laptop.Id, 3);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Error: " + ex.Message);
+}
+var rental = db.Rentals[0];
+Console.WriteLine("Returning equipment");
+var penalty = service.ReturnEquipment(rental.Id);
+Console.WriteLine("Penalty: " + penalty);
+Console.WriteLine("report");
+Console.WriteLine(service.GetSummaryReport());
