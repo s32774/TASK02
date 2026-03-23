5a4eadd085550c3d217fea498969aa7cdaf4230b
Olena Chornobai <s32774@pjwstk.edu.pl>
2026-03-23 18:14:32 +0100

    fixed the interface

--- Interfaces/IRentalService.cs
+++ Interfaces/IRentalService.cs
@@ -1,17 +1,17 @@
 ﻿using APBD_TASK2.Models;
-
-namespace APBD_TASK2.Interfaces;
-
-public interface IRentalService
+namespace APBD_TASK2.Interfaces
 {
-    void AddUser(User user);
-    void AddEquipment(Equipment equipment);
-    List<Equipment> GetAllEquipment();
-    List<Equipment> GetAvailableEquipment();
-    void RentEquipment(Guid userId, Guid equipmentId, int days);
-    decimal ReturnEquipment(Guid rentalId);
-    void MarkEquipmentUnavailable(Guid equipmentId);
-    List<Rental> GetActiveRentalsForUser(Guid userId);
-    List<Rental> GetOverdueRentals();
-    string GetSummaryReport();
+    public interface IRentalService
+    {
+        void AddUser(User user);
+        void AddEquipment(Equipment equipment);
+        List<Equipment> GetAllEquipment();
+        List<Equipment> GetAvailableEquipment();
+        void RentEquipment(Guid userId, Guid equipmentId, int days);
+        decimal ReturnEquipment(Guid rentalId);
+        void MarkEquipmentUnavailable(Guid equipmentId);
+        List<Rental> GetActiveRentalsForUser(Guid userId);
+        List<Rental> GetOverdueRentals();
+        string GetSummaryReport();
+    }
 }
